37f26d5ab42eb29b059de86fc90c585d48e37c7f
Salman Alfarizi <salmanlfaa11@gmail.com>
2025-09-19 18:26:05 +0700

    design model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,37 +12,23 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $table = 'users';
+    protected $primaryKey = 'idusers';
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'email', 'password', 'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password'];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    // Relasi
+    public function patient()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasOne(Patient::class, 'patient_id', 'idusers');
+    }
+
+    public function doctor()
+    {
+        return $this->hasOne(Doctor::class, 'doctor_id', 'idusers');
     }
 }
